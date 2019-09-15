0b211cd88255eaeab093d2906369dc6fb7e28b9b
MatthewSong86 <54823079+MatthewSong86@users.noreply.github.com>
2019-09-15 16:25:21 -0700

    Update Autonomous.java

--- Autonomous.java
+++ Autonomous.java
@@ -1,4 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Autonomous {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Autonomous extends LinearOpMode {
+    
+
+    
+    public void runOpMode() {
+
+    }
 }
