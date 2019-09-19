dec1fc1cd01ea97e95062a7c951feb319effeaae
MatthewSong86 <54823079+MatthewSong86@users.noreply.github.com>
2019-09-18 20:44:31 -0700

    Add files via upload

---
+++ AutonCode3.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name="AutonCode3", group="example")
+public class AutonCode3 extends LinearOpMode {
+    private DcMotor frontLeft, backLeft, frontRight, backRight;
+    public void runOpMode(){
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+
+    }
+
+}
