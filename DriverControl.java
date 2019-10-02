2449793ebc82ad186b65ea5ea70f7deb3c338d39
MatthewSong86 <54823079+MatthewSong86@users.noreply.github.com>
2019-10-01 21:04:39 -0700

    Update DriverControl.java

--- DriverControl.java
+++ DriverControl.java
@@ -2,27 +2,31 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
-public class DriverControl extends LinearOpMode {
-    //initiating motors
+public class DriverControl extends LinearOpMode{
+
     private DcMotor frontLeft;
     private DcMotor frontRight;
     private DcMotor backLeft;
     private DcMotor backRight;
-    //private DcMotor m1,m2,m3,m4; //these motors will be used for other things
+    private Servo platform;
     public void runOpMode(){
         frontLeft  = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRight  = hardwareMap.get(DcMotor.class, "frontRight");
         backLeft  = hardwareMap.get(DcMotor.class, "backLeft");
         backRight  = hardwareMap.get(DcMotor.class, "backRight");
+        platform = hardwareMap.get(Servo.class, "platform");
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
         waitForStart();
-        double fbLeftStick=0; 
+        double fbLeftStick=0;
         double lrLeftStick=0;
         double lrRightStick=0;
         double fbRightStick=0;
+        double ppUp=0; //subject to change
+        double ppDown=0; // subject to change
         frontRight.setDirection(DcMotor.Direction.REVERSE);
         backRight.setDirection(DcMotor.Direction.REVERSE);
         //put all movement code in here
@@ -31,31 +35,37 @@ public class DriverControl extends LinearOpMode {
             fbRightStick = -this.gamepad1.right_stick_y;
             lrLeftStick = -this.gamepad1.left_stick_x; //if turning the wrong direction reverse the sign
             lrRightStick = -this.gamepad1.right_stick_x; //if turning the wrong direction reverse the sign
-            /*
+            if(this.gamepad1.left_trigger>0.1){
+                platformUp(ppUp);
+            }
+            else if(this.gamepad1.left_bumper==true){
+                platformDown(ppDown);
+            }
             movingFB(fbLeftStick);
-            strafe(lrLeftStick);
+            //strafe(lrLeftStick);
             turn(lrRightStick);
-            */
+
             //tank(fbLeftStick, fbRightStick);
             telemetry.addData("Status", "Running");
             telemetry.update();
         }
     }
     public void tank(double fbLeftStick, double fbRightStick){
+
         frontLeft.setPower(fbLeftStick);
         backLeft.setPower(fbLeftStick);
         backRight.setPower(fbRightStick);
-        frontRight.setPower(fbRightstick);
+        frontRight.setPower(fbRightStick);
         telemetry.addData("Target Power", fbLeftStick);
         telemetry.addData("Target Power", fbRightStick);
         telemetry.addData("Motor Power", frontLeft.getPower());
         telemetry.addData("Motor Power", frontRight.getPower());
         telemetry.addData("Motor Power", backLeft.getPower());//hi test
         telemetry.addData("Motor Power", backRight.getPower());
-        
+
     }
     public void movingFB(double fbLeftStick){
-        
+
         frontLeft.setPower(fbLeftStick);
         frontRight.setPower(fbLeftStick);
         backLeft.setPower(fbLeftStick);
@@ -66,8 +76,9 @@ public class DriverControl extends LinearOpMode {
         telemetry.addData("Motor Power", backLeft.getPower());
         telemetry.addData("Motor Power", backRight.getPower());
     }
+
     public void strafe(double lrLeftStick){
-        
+
         frontLeft.setPower(lrLeftStick);
         backLeft.setPower(-lrLeftStick);
         frontRight.setPower(-lrLeftStick);
@@ -79,7 +90,7 @@ public class DriverControl extends LinearOpMode {
         telemetry.addData("Motor Power", backRight.getPower());
     }
     public void turn(double lrRightStick){
-        
+
         frontLeft.setPower(lrRightStick);
         backLeft.setPower(lrRightStick);
         frontRight.setPower(-lrRightStick);
@@ -90,9 +101,18 @@ public class DriverControl extends LinearOpMode {
         telemetry.addData("Motor Power", backLeft.getPower());
         telemetry.addData("Motor Power", backRight.getPower());
     }
-    /*
-    public void m1(double m1){
+
+    public void platformUp(double plat){
+        platform.setPosition(plat);
+        telemetry.addData("Target Power", plat);
+        telemetry.addData("Servo Power", platform.getPosition());
     }
+    public void platformDown(double plat){
+        platform.setPosition(plat);
+        telemetry.addData("Target Power", plat);
+        telemetry.addData("Servo Power", platform.getPosition());
+    }
+    /*
     public void m2(double m2){
     }
     public void m3(double m3){
